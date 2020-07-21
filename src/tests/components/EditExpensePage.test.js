0290b522e46b6fe532dab13950db0a32827c0c80
Rachel Schlater <rachelschlater@Rachel-Schlaters-MacBook-Pro.local>
2020-07-21 14:03:48 -0400

    Add startEditExpense

--- src/tests/components/EditExpensePage.test.js
+++ src/tests/components/EditExpensePage.test.js
@@ -4,15 +4,15 @@ import { EditExpensePage } from '../../components/EditExpensePage';
 import expenses from '../fixtures/expenses';
 
 
-let editExpense, startRemoveExpense, history, wrapper;
+let startEditExpense, startRemoveExpense, history, wrapper;
 
 beforeEach(() => {
-    editExpense = jest.fn();
+    startEditExpense = jest.fn();
     startRemoveExpense = jest.fn();
     history = { push: jest.fn() };
     wrapper = shallow(
         <EditExpensePage
-         editExpense={editExpense}
+        startEditExpense={startEditExpense}
          history={history}
          startRemoveExpense={startRemoveExpense}
          expense={expenses[1]}
@@ -26,7 +26,7 @@ test('should render EditExpensePage', () => {
 test('should handle editExpense', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1])
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(editExpense).toHaveBeenLastCalledWith(expenses[1].id, expenses[1])
+    expect(startEditExpense).toHaveBeenLastCalledWith(expenses[1].id, expenses[1])
 })
 
 test('should handle startRemoveExpense', () => {
