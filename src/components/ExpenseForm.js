d3b835155d6dccbcd2c8ac5bc813c01a3898a570
Rachel Schlater <rachelschlater@Rachel-Schlaters-MacBook-Pro.local>
2020-09-28 17:32:27 -0400

    Fix ExpenseForm note props

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,7 +9,7 @@ export default class ExpenseForm extends React.Component {
         super(props);
         this.state = {
             description: props.expense ? props.expense.description : '',
-            note: props.note ? props.expense.note : '',
+            note: props.expense ? props.expense.note : '',
             amount: props.expense ? (props.expense.amount / 100).toString() : '',
             createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
             calendarFocused: false,
